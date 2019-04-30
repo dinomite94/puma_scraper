2aa33670ab00d9d8b438701b966feeb2b4399ed0
Tobias Kruse <tobias.kruse@mailbox.org>
2019-04-30 16:47:17 +0200

    added excel export, cli and puma assets

---
+++ jobs/cli.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3
+import argparse
+import json
+import os
+import shutil
+import subprocess
+import sys
+
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+out_dir = os.path.join(root_dir, 'out')
+out_jobs_dir = os.path.join(out_dir, 'jobs')
+
+
+def clean(args):
+    pass
+
+
+def compress(args):
+    shutil.make_archive(out_jobs_dir, 'zip', out_jobs_dir)
+
+
+def crawl(args):
+    pass
+
+
+def process(args):
+    for filename in os.listdir(out_jobs_dir):
+        full_filename = os.path.join(out_jobs_dir, filename)
+        with open(full_filename, 'r') as f:
+            data = json.load(f)
+
+
+cmds = {
+    'clean': clean,
+    'compress': compress,
+    'crawl': crawl,
+    'process': process,
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Puma Job Offers Crawler')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers.required = False
+    
+    subparsers.add_parser('clean', help='Clean the output directory')
+    subparsers.add_parser('compress', help='Create archive of output directory')
+
+    crawl_parser = subparsers.add_parser('crawl', help='Run scrapy crawler for new data')
+    crawl_parser.add_argument('--url', help='List of job openings (default: https://about.puma.com/en/careers/job-openings#jobfinder)')
+    crawl_parser.add_argument('-no-html', type=bool, help='Do NOT save the HTML website')
+    crawl_parser.add_argument('-no-screenshots', type=bool, help='Do NOT save the screenshots')
+    crawl_parser.add_argument('-no-json', type=bool, help='Do NOT save JSON data')
+
+    process_parser = subparsers.add_parser('process', help='Process crawled data')
+    process_parser.add_argument('--process', type=bool, help='Process crawled data')
+    process_parser.add_argument('-offline-html', type=bool, help='Preparate saved HTML website to work offline')
+    process_parser.add_argument('-export-xlsx', type=bool, help='Process saved JSON data and fill Excel Sheet')
+
+    args = parser.parse_args()
+
+    if args:
+        cmds[sys.argv[1]](args)
---
+++ jobs/excel_exporter.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python3
+from openpyxl import workbook, load_workbook
+from openpyxl.drawing.image import Image
+
+from collections import namedtuple
+import os
+
+
+HyperLink = namedtuple('HyperLink', ['text', 'link'])
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+template_xlsx = os.path.join(root_dir, 'templates/Auswertung Untersuchungskriterien.xlsx')
+output_xlsx = os.path.join(root_dir, 'out/Auswertung Untersuchungskriterien.xlsx')
+
+
+def export(data_table):
+    xfile = load_workbook(template_xlsx)
+    sheet = xfile.get_sheet_by_name('Sheet1')
+
+    row_num = 5
+    for row in data_table:
+        column_num = 2
+
+        for data in row:
+            cell = sheet.cell(row=row_num, column=column_num)
+
+            if isinstance(data, HyperLink):
+                cell.value = data.text
+                cell.hyperlink = data.link
+                cell.style = 'Hyperlink'
+            else:
+                cell.value = data
+
+            column_num += 1
+
+        row_num += 1
+
+    xfile.save(output_xlsx)
+
+
+
+if __name__ == "__main__":    
+    rows = [
+        [0, 0, 0,
+            HyperLink(text='Screenshot', link='jobs/r874.png'),
+            HyperLink(text='Lokal', link='jobs/r874.html'),
+            HyperLink(text='Online', link='https://about.puma.com/en/jobs/r874')],
+        [1, 2, 3],
+        [6, 5, 4],
+    ]
+
+    export(rows)
